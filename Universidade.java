fd6c71de48acdc98d45d874b8657f52b9a9b36a0
Bettina_Chaves <bettina_chaves@dellteam.com>
2024-04-29 18:35:14 -0300

    limpando a pasta

--- Universidade.java
+++ Universidade.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SimuladorUniversidade {
+public class Universidade {
 
     public static void main(String[] args) {
         // Ler a entrada do arquivo
