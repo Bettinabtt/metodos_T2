7f3e1037321f5d0ad51d52bbf9f474eac815a472
Bettina_Chaves <bettina_chaves@dellteam.com>
2024-04-30 09:40:00 -0300

    mudando o metodo de simular

--- Universidade.java
+++ Universidade.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Universidade {
@@ -39,30 +41,52 @@ public class Universidade {
     }
 
     public static int simularUniversidade(int numAlunosSem1, Map<String, Double> transicoes) {
-        int numAlunosDiplomados = 0;
-        int numAlunosSemestre = numAlunosSem1;
+    int numAlunosDiplomados = 0;
+    int numAlunosSemestre = numAlunosSem1;
+    String semestreAtual = "Sem_1";
 
-        while (!transicoes.containsKey("Diploma 1")) {
-            int numAlunosProximoSemestre = 0;
-            for (Map.Entry<String, Double> entry : transicoes.entrySet()) {
-                String transicao = entry.getKey();
-                double probabilidade = entry.getValue();
-
-                String[] semestres = transicao.split(" -> ");
-                String proximoSemestre = semestres[1];
-
-                int alunosNesteSemestre = (int) (numAlunosSemestre * probabilidade);
-                if (proximoSemestre.equals("Diploma 1")) {
-                    numAlunosDiplomados += alunosNesteSemestre;
-                } else {
-                    numAlunosProximoSemestre += alunosNesteSemestre;
-                }
+    while (!semestreAtual.startsWith("Diploma")) {
+        List<String> transicoesPossiveis = new ArrayList<>();
+        for (String transicao : transicoes.keySet()) {
+            String[] partes = transicao.split(" -> ");
+            if (partes[0].equals(semestreAtual)) {
+                transicoesPossiveis.add(transicao);
             }
-            numAlunosSemestre = numAlunosProximoSemestre;
         }
 
-        return numAlunosDiplomados;
+        // Selecionar uma transição possível com base nas probabilidades
+        String proximoSemestre = selecionarProximoSemestre(transicoesPossiveis, transicoes);
+
+        if (!proximoSemestre.startsWith("Diploma")) {
+            int alunosNesteSemestre = (int) (numAlunosSemestre * transicoes.getOrDefault(proximoSemestre, 0.0));
+            numAlunosSemestre -= alunosNesteSemestre;
+        } else {
+            numAlunosDiplomados += numAlunosSemestre;
+            numAlunosSemestre = 0;
+        }
+
+        semestreAtual = proximoSemestre;
     }
+
+    return numAlunosDiplomados;
+}
+
+private static String selecionarProximoSemestre(List<String> transicoesPossiveis, Map<String, Double> transicoes) {
+    double valorAleatorio = Math.random();
+    double acumulado = 0.0;
+
+    for (String transicao : transicoesPossiveis) {
+        acumulado += transicoes.get(transicao);
+        if (valorAleatorio <= acumulado) {
+            return transicao.split(" -> ")[1];
+        }
+    }
+
+    // Se não for selecionado, retornar o último como padrão
+    return transicoesPossiveis.get(transicoesPossiveis.size() - 1).split(" -> ")[1];
+}
+
+    
 
     public static int calcularTotalAlunos(Map<String, Double> transicoes, int numAlunosSem1, int numAlunosDiplomados) {
         int numAlunosTotal = numAlunosSem1;
